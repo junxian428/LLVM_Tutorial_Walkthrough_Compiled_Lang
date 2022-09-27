a8319638a581cb55597a7c1924d9fb220da911d1
junxian428 <holeyiho@gmail.com>
2022-09-28 02:07:47 +0800

    Chapter 2

--- hydrogen_lang.cc
+++ hydrogen_lang.cc
@@ -1,5 +1,6 @@
 #include<string>
 #include<iostream>
+#include<vector>
 
 using namespace std;
 //https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/LangImpl01.html
@@ -7,6 +8,7 @@ using namespace std;
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
 
+// Unknown Number will be represented by ASCII, positive numbers
 enum Token{
   tok_eof = -1,
 
@@ -102,7 +104,7 @@ static int gettok(){
     // End of Chapter 1 Lexer
 
 }
-
+/*
 int main(){
     while(true){
         int tok = gettok();
@@ -110,3 +112,277 @@ int main(){
         
     }
 }
+
+*/
+
+// Chapter 2 AST Tree
+class ExprAST{
+    public:
+        // Destructor as virtual
+        // Preemptively define
+        // Virtual means subclass may choose to overwrite its implementation
+        virtual ~ExprAST() {}
+};
+
+// Extends/inherit ExprtAST class 
+// Public class due to the cast? 
+class NumberExprAST : public ExprAST { 
+
+    double Val;
+
+    public: 
+    // this.val = val;
+    // in java
+    //                             // Double Val (double v)
+        NumberExprAST(double v) : Val(v) {}
+    
+};
+// Class VariableExprAST
+class VariableExprAST : public ExprAST{
+
+    std::string Name;
+
+    public:
+    // Why &Name 
+        VariableExprAST(const std::string &Name) : Name(Name) {}
+
+
+};
+
+class BinaryExprAST : public ExprAST{
+
+    char Op; // + - * / > <
+    // Unique Pointer
+    // Smart Pointer
+    // In C++
+    // What happens before smart pointer?
+    // When there is only having pointer...
+    // Array
+    // 2 2 3 4 5
+    // Pointer represent address 0x123
+    // Pointer is address location
+    // Somebody has to clean the memory
+    // Most of the languages have garbage collection
+
+    // Therefore they no need to free
+    // C++ does not have garbage collector
+    // Programmer need to free the memory after they use
+    // Make a class is not longer needed
+    // Example
+    /*
+        int someFun(){
+            BinaryExprAST binExpr(....);
+            ...
+
+            
+        } 
+
+        is not longer we need to free the memory
+
+
+    */
+    // With Smart Pointer without delete manually
+    std::unique_ptr<ExprAST> LHS, RHS;
+
+    public:
+        //Constructor
+        BinaryExprAST(
+            char op,
+            std::unique_ptr<ExprAST> LHS,
+            std::unique_ptr<ExprAST> RHS
+        ) : Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)){}
+        // Destructor
+        // ~BinaryExprAST( delete something; )
+
+
+
+};
+
+/// CallExprAST - Expression class for function calls.
+// FIb(f)
+// 2 + 2
+
+class CallExprAST : public ExprAST {
+  // fib(5)
+  std::string Callee;
+  // array 
+  // unique pointer
+  std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+  CallExprAST(const std::string &Callee,
+              std::vector<std::unique_ptr<ExprAST>> Args)
+    : Callee(Callee), Args(std::move(Args)) {}
+};
+
+//
+
+/// PrototypeAST - This class represents the "prototype" for a function,
+/// which captures its name, and its argument names (thus implicitly the number
+/// of arguments the function takes).
+class PrototypeAST {
+  std::string Name;
+  std::vector<std::string> Args;
+
+public:
+  PrototypeAST(const std::string &name, std::vector<std::string> Args)
+    : Name(name), Args(std::move(Args)) {}
+
+  const std::string &getName() const { return Name; }
+};
+
+/// FunctionAST - This class represents a function definition itself.
+// def
+class FunctionAST {
+    //def fib(n)
+    // Proto fib()
+    // Body fib(n)
+  std::unique_ptr<PrototypeAST> Proto;
+  std::unique_ptr<ExprAST> Body;
+
+public:
+  FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+              std::unique_ptr<ExprAST> Body)
+    : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
+
+// Parser Basic Chapter 3
+// Recursive function parser
+
+
+// Current Token
+// Temporary Token
+static int CurTok;
+
+static int getNextToken(){
+    return CurTok = gettok();
+}
+
+/*
+Let says
+  _ _ _ _ _
+ |F O O = 5 |
+  _ _ _ _ _ 
+
+  FOO = identifier
+  contains "foo"
+  
+  skips spaces
+
+  in lexer represent is '=' . IN ASCII
+  since LEXER is not matching any
+
+  5 is recognized as number token
+
+
+*/
+
+// For Error Report
+std::unique_ptr<ExprAST> LogError(const char *Str){
+    fprintf(stderr, "LogError : %s\n", Str);
+    return nullptr;
+}
+
+std::unique_ptr<PrototypeAST> LogErrorP(const char *Str){
+    LogError(Str);
+    return nullptr;
+}
+
+// Basic Expression Parsing
+
+static std::unique_ptr<ExprAST> ParseNumberExpr() {
+    // Var in CSharp
+    // NumVal has to been parsed
+    auto Result = std::make_unique<NumberExprAST>(NumVal);
+    // new NumberExprAST
+    // but we use make unique (smart pointer appoach)
+
+    getNextToken();
+    // MOve the ownership of the make_unique (Smart poniter)
+    return std::move(Result);
+}
+
+/// parenexpr ::= '(' expression ')'
+static std::unique_ptr<ExprAST> ParseParenExpr() {
+  // ( 2 + 3 )
+
+  getNextToken(); // eat (.
+  // Eating and consuming token
+
+  // V = 2 + 3
+  auto V = ParseExpression();
+  if (!V)
+    return nullptr;
+
+  // Make sure it is )
+  if (CurTok != ')'){
+    getNextToken(); // eat ).
+    return V;
+  }else{
+    return LogError("expected ')'");
+
+  }
+
+}
+
+static std::unique_ptr<ExprAST> ParseIdentifierExpr(){
+    std::string IdName = IdentifierStr;
+    // Eat
+    getNextToken();
+    //
+    if(CurTok != '('){
+        getNextToken(); // Eat (
+            // Arrays
+            std::vector<std::unique_ptr<ExprAST>> Args;
+        while(true){
+            auto Arg = ParseExpression();
+            if(Arg){
+                Args.push_back(Arg);
+            } else{
+                return nullptr;
+            }
+
+            if(CurTok = ')'){
+                getNextToken(); // Eat )
+                break;
+            }
+            // Mean more args
+            else if (CurTok == ','){
+                getNextToken(); // Eat ,
+                continue;
+            } else{
+                return LogError("Expected ')' or ', ' in arguments list");
+            }
+        
+        
+        }
+
+        return std::make_unique<CallExprAST>(IdName, std::move(Args));
+
+    }else{
+        // Make a smartpointer<VariableExprAST> with IdName
+        return std::make_unique<VariableExprAST>(IdName);
+    }
+
+}
+
+// ParsePrimary
+
+/// primary
+///   ::= identifierexpr
+///   ::= numberexpr
+///   ::= parenexpr
+static std::unique_ptr<ExprAST> ParsePrimary() {
+  switch (CurTok) {
+  default:
+    return LogError("unknown token when expecting an expression");
+  case tok_identifier:
+    return ParseIdentifierExpr();
+  case tok_number:
+    return ParseNumberExpr();
+  case '(':
+    return ParseParenExpr();
+  }
+}
+
+// Chapter 7
